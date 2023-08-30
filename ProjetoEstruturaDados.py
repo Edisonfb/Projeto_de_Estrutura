bbe639aa4383b2e7d5a08655b4b298d943bda834
Carol Damasceno <caroldamascenoal@gmail.com>
2023-08-30 19:01:18 -0300

    adicionando classe filmes

--- ProjetoEstruturaDados.py
+++ ProjetoEstruturaDados.py
@@ -3,6 +3,26 @@
 import pandas as pd
 import Functions as func
 
+class Filmes:
+   #Construtor da classe com as variáveis
+   def __init__(self,budget,genres,id,overview,orinal_Language,popularity,production_companies,release_Date,revenue,runtime,tagline,title,vote_average,vote_count):
+       self.budget = budget
+       self.genres = genres
+       self.id = id
+       self.overview = overview
+       self.original_Language = orinal_Language
+       self.popularity = popularity
+       self.production_companies = production_companies
+       self.release_Date = release_Date
+       self.revenue = revenue
+       self.runtime = runtime
+       self.tagline = tagline
+       self.title = title
+       self.vote_average = vote_average
+       self.vote_count = vote_count#Criando objeto da classe filme     
+meu_Filme = Filmes(0,"",0,"","",0,"","",0.0,0,"","",0,0)
+
+
 
 # essa função lê a planilha para dentro da variável data como um DataFrame
 # (diferente da lista e string), então para modificar e fazer as alterações deve-se
@@ -13,3 +33,4 @@ data = pd.read_csv("Popular_Movies.csv", delimiter=";")
 
 # printar a tabela, só que ela é muito grande e precisa ser ajustada
 print(data)
+
